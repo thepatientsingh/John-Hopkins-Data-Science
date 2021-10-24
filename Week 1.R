c11d2d924a37585df9df81b6ae2c981758156018
thepatientsingh <singhdhiraj2291@outlook.com>
2021-10-24 17:01:36 +0530

    Week 1

---
+++ Week 1.R
@@ -0,0 +1,18 @@
+x=list(1,'a',T,1+6i)
+print(x)
+
+# Creating Matrices
+b=matrix(1:10,nrow = 5)
+print(b)
+
+a=1:10
+dim(a)=c(2,5)
+print(a)
+
+c=1:3
+d=4:6
+cd=rbind(c,d)
+dc=cbind(c,d)
+print(cd)
+print(dc)
+
